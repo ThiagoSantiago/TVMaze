61b7edf03e62105b31893495b83dc47d1dd8b597
Thiago Santiago <thiagoalexandres@gmail.com>
2024-12-22 16:28:13 -0300

    Initial Commit

---
+++ TVMaze/TVMazeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TVMazeApp.swift
+//  TVMaze
+//
+//  Created by Thiago Santiago on 22/12/24.
+//
+
+import SwiftUI
+
+@main
+struct TVMazeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TVMazeTests/TVMazeTests.swift
@@ -0,0 +1,17 @@
+//
+//  TVMazeTests.swift
+//  TVMazeTests
+//
+//  Created by Thiago Santiago on 22/12/24.
+//
+
+import Testing
+@testable import TVMaze
+
+struct TVMazeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
